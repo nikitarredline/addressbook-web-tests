408c4abf605512dd528715998ade575249fa846b
nikitarredline <52927806+nikitarredline@users.noreply.github.com>
2020-11-05 02:06:07 +0300

    Initial commit

---
+++ src/test/java/ru/stqa/pft/addressbook/GroupData.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.addressbook;
+
+public class GroupData {
+    private final String name;
+    private final String header;
+    private final String footer;
+    private final String email;
+
+    public GroupData(String name, String header, String footer, String email) {
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getFooter() {
+        return footer;
+    }
+
+    public String getEmail() { return email; }
+}
