a9a463041c7776d19a8531be72de021d74b8660f
nikitarredline <52927806+nikitarredline@users.noreply.github.com>
2020-12-13 13:44:38 +0300

    Задание №15

--- src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
+++ src/test/java/ru/stqa/pft/addressbook/tests/DbConnectionTest.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
